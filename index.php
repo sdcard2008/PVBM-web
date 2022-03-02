83cda05f32a96418875a53b0e060ecfe8f3780b8
sdcard2008 <100673474+sdcard2008@users.noreply.github.com>
2022-03-02 11:51:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PVBM Jhargram-Home</title>
+        <meta name="viewport" content="width=device-width , initial-scale=1">
+        <meta name = 'author'
+        content = 'Saptak De'/>
+        <meta charset="UTF-8"/>
+        <meta name = 'description'
+        content = 'Website of a Paschim Vangya Bigyan Mancha Jhargram'/>
+        <meta name = 'keyword'
+        content ='science , global warming , science activities'/>
+        <link href="assets/css/index-style.css" type='text/css' rel='stylesheet'>
+        <link href='assets/css/universal-template.css' type='text/css' rel='stylesheet'>
+        
+    </head>
+    <body>
+        <div id='box'>
+            <img class="logo" src=assets/css/image/logo.png>
+        </div>
+        <h1 class = 'title'>Paschim vangya bigyan mancha</h1>
+        <h2 class = 'title'>Jhargram Jilla Comitte</h2>
+        <h3 class = 'title'>Home</h3>
+        <div id = 'toolbar'>
+            <ul>
+                <li class = 'tools'><a href = 'index.php'>Home</a></li>
+                <li class = 'tools'><a href = 'assets/about.php'>About Us</a></li>
+                <li class = 'tools'><a href = 'assets/read.php'>Our magazines</a></li>
+                <li class = 'tools'><a href = 'assets/join.php'>Join Us</a></li>
+                <li class = 'tools'><a href = 'assets/feedback.php'>Feedback</a></li>
+                <li class = 'tools'><a href='assets/admin_login.php'>Admin Login</a></li>
+                <li class = 'tools'><a href='assets/notice_page.php'>Notice And Events</a></li>
+                <li class='tools'><a href='assets/exam_enroll'>Exam Enrollment</a></li>
+                
+            </ul>
+        </div>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        
+        <h2 class='title'>Some photoes of our ceremonies</h2>
+        <div id='photo_gallery'>
+            
+            <div class='ph_bx'><img class='ph_img' src=assets/css/image/Desert.jpg /> </div>
+            <div class='ph_bx'></div>
+            <div class='ph_bx'></div>
+            <div class='ph_bx'></div>
+            <div class='ph_bx'></div>
+            <!And so on>
+        </div>
+        <br/>
+        <br/>
+        <div id = 'adress_box'>
+            <h2 id = 'title_adress'>Address</h2>
+            <address>
+                <p class='adress'>Raghunathpur , Jhargram</p>
+                <p class='adress'>P.O Jhargram</p>
+                <p class='adress'>District Jhargram</p>
+                <p class='adress'>Pin - 721507</p>
+            </address>
+        </div>    
+    
+
+    </body>
+</html>
+
